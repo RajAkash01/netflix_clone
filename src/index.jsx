0a123c82fb032936f97249d803f4398ee1afb0fa
RajAkash01 <raj1006akash@gmail.com>
2026-04-01 12:54:21 +0530

    updated public url

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/netflix_clone">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
